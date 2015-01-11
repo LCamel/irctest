ba2f174cd3a77383c43d01209a6b944320543514
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2015-01-11 16:54:26 +0800

    add server side code for WebSocket (https://www.playframework.com/documentation/2.3.x/ScalaWebSockets)

---
+++ app/actors/MyWebSocketActor.scala
@@ -0,0 +1,15 @@
+package actors
+
+import akka.actor._
+
+object MyWebSocketActor {
+  def props(out: ActorRef) = Props(new MyWebSocketActor(out))
+}
+
+class MyWebSocketActor(out: ActorRef) extends Actor {
+  def receive = {
+    case msg: String =>
+      out ! ("I received your message: " + msg)
+  }
+}
+
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,10 +3,17 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import play.api.Play.current
+
+import actors.MyWebSocketActor
+
 object Application extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
 
+  def ws = WebSocket.acceptWithActor[String, String] { request => out =>
+    MyWebSocketActor.props(out)
+  }
 }
