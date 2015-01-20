cf7fd78b9650a612cd016c8e74c6436bfb21508f
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2015-01-21 01:03:39 +0800

    redirect http to https on production env (http://stackoverflow.com/questions/19147147/best-way-to-force-playframework-2-to-always-use-ssl)

--- app/Global.scala
+++ app/Global.scala
@@ -1,10 +1,14 @@
 import play.api.GlobalSettings
 import play.api.Application
 import irctest.MyAgent
+import play.api.mvc.Handler
+import play.api.mvc.RequestHeader
+import play.api.Play
+import play.api.Play.current
 
 object Global extends GlobalSettings {
   println("==== Global init!")
-  
+
   override def onStart(app: Application) {
     println("==== Global onStart()")
     MyAgent.onStart()
@@ -14,4 +18,16 @@ object Global extends GlobalSettings {
     println("==== Global onStop()")
     MyAgent.onStop()
   }
+
+
+  // http://stackoverflow.com/questions/19147147/best-way-to-force-playframework-2-to-always-use-ssl
+  // https://www.playframework.com/documentation/2.3.x/Production
+  override def onRouteRequest(request: RequestHeader): Option[Handler] = {
+    if (Play.isProd && !request.secure) {
+      Some(controllers.Secure.redirect)
+    } else {
+      super.onRouteRequest(request)
+    }
+  }
+
 }
---
+++ app/controllers/Secure.scala
@@ -0,0 +1,11 @@
+package controllers
+
+import play.api.mvc._
+
+object Secure extends Controller {
+
+  def redirect = Action { implicit request =>
+    //MovedPermanently("https://" + request.host + request.uri)
+    TemporaryRedirect("https://" + request.host + request.uri)
+  }
+}
